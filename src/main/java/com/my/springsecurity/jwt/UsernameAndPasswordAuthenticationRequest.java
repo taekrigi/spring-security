bd9208c2d3ce561811dd4798f7f8fdd49a11c948
taekrigi <taekrigi@taekrigiui-MacBookPro.local>
2020-12-29 20:25:54 -0800

    set https

--- src/main/java/com/my/springsecurity/jwt/UsernameAndPasswordAuthenticationRequest.java
+++ src/main/java/com/my/springsecurity/jwt/UsernameAndPasswordAuthenticationRequest.java
@@ -5,10 +5,8 @@ public class UsernameAndPasswordAuthenticationRequest {
     private String username;
     private String password;
 
-
     public UsernameAndPasswordAuthenticationRequest() {
     }
-
 
     public UsernameAndPasswordAuthenticationRequest(String username, String password) {
         this.username = username;
